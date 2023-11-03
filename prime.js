02304ecaf2eed6f8aaf62d7a253193a481c80d91
FionaV2400 <139073560+FionaVerzivolli@users.noreply.github.com>
2023-11-02 21:39:27 -0400

    Update prime.js

--- prime.js
+++ prime.js
@@ -1,61 +1,84 @@
+// Function to calculate factors of a number
 function factorization(num) {
     const factors = [];
     for (let i = 2; i <= Math.floor(Math.sqrt(num)); i++) {
         while (num % i === 0) {
-            factors.push(i);
-            num /= i;
+            factors.push(i); // Push the factor into the factors array
+            num /= i; // Reduce the number by dividing it by the factor
         }
     }
     if (num > 1) {
         factors.push(num);
     }
-    return factors;
+    return factors; // Return the array of factors
 }
 
+// Function to generate the factor tree visualization
 function generateFactorTree() {
     const input = document.getElementById('inputNumber');
     const number = parseInt(input.value);
     const factorTreeContainer = document.getElementById('factorTree');
-    factorTreeContainer.innerHTML = '';
+    factorTreeContainer.innerHTML = ''; // Clear the factor tree container
 
+    // Check if the input is valid
     if (isNaN(number) || input.value.length > 15 || number < 1) {
         alert('Please enter a valid number!');
     } else {
-        let factors = factorization(number);
+        let factors = factorization(number); // Get the factors of the number
         let num = number;
-        factorTreeContainer.innerHTML += `<p>${num}</p>`;
+        factorTreeContainer.innerHTML += `<p>${num}</p>`; // Display the original number
+
+        // Build the factor tree
         for (let i = 0; i < factors.length; i++) {
-            num /= factors[i];
+            num /= factors[i]; // Divide the number by each factor
             if (num > 1) {
-            factorTreeContainer.innerHTML += `&darr;</br> ${num} x ${factors[i]}</p> `;
+                factorTreeContainer.innerHTML += `&darr;</br> ${num} x ${factors[i]}</p> `; // Display factor tree node
             } else {
                 break;
             }
         }
     }
-    factorTreeContainer.innerHTML += `We can see that the prime factors are the right-most numbers. <br></br> Verify this by clicking 'Generate!'`
+    factorTreeContainer.innerHTML += `We can see that the prime factors are the right-most numbers. <br></br> Verify this by clicking 'Generate!'`;
 }
 
+// Function to generate and display the unique prime factors
 function generateFactors() {
     const input = document.getElementById('inputNumber');
     const number = parseInt(input.value);
+    const resultDiv = document.getElementById('result');
+    resultDiv.innerHTML = ''; // Clear the result container
 
+    // Check if the input is valid
     if (isNaN(number) || input.value.length > 15 || number < 1) {
         alert('Please enter a valid number!');
         input.value = '';
         return;
     }
 
-    const factors = factorization(number);
-    const resultDiv = document.getElementById('result');
-    resultDiv.innerHTML = `Prime factor(s): ${factors.join(', ')}`;
+    const factors = factorization(number); // Get all factors
+    const uniqueFactors = Array.from(new Set(factors)); // Get unique factors
+
+    // Display the unique prime factors or indicate if the number is a prime
+    if (uniqueFactors.length === 0) {
+        resultDiv.innerHTML = `The number ${number} is a prime number`;
+    } else {
+        resultDiv.innerHTML = `</br></br>Prime factor(s): ${uniqueFactors.join(', ')}`;
+    }
 }
-document.addEventListener('DOMContentLoaded', function() {
+
+// Event listener for page load
+document.addEventListener('DOMContentLoaded', function () {
     const generateBtn = document.querySelector('button[onclick="generateFactors()"]');
     const visualizeBtn = document.querySelector('button[onclick="generateFactorTree()"]');
 
-    generateBtn.addEventListener('click', generateFactors);
-    visualizeBtn.addEventListener('click', generateFactorTree);
+    // Event listeners for 'Generate' and 'Visualize' buttons
+    generateBtn.addEventListener('click', function () {
+        generateFactors(); // Generate unique prime factors
+        document.getElementById('factorTree').innerHTML = ''; // Clear factor tree container
+    });
+
+    visualizeBtn.addEventListener('click', function () {
+        generateFactorTree(); // Generate the factor tree visualization
+        document.getElementById('result').innerHTML = ''; // Clear result container
+    });
 });
-
-
